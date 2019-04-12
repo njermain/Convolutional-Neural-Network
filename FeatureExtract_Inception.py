982bc7a63e0e42042eea4e1c7d862d45d574388b
Nate Jermain <45343674+njermain@users.noreply.github.com>
2019-04-12 13:13:00 -0500

    Add files via upload

--- FeatureExtract_Inception.py
+++ FeatureExtract_Inception.py
@@ -13,46 +13,53 @@ import pandas as pd
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
+# read in pretrained model and remove softmax layer
 model = InceptionV3(weights='imagenet', include_top=False)
 model.summary()
 
+# empty list to add features for each sample to
 inception_feature_list = []
 main_dir='C:/Users/w10007346/Dropbox/CNN/Unsupervised_Set/'
 from os import listdir
 from os.path import isfile, join
-
+# get the file names from the directory
 files=[f for f in listdir(main_dir) if isfile(join(main_dir, f))]
 
 for file in files:
     fullpath=main_dir+file
     img = image.load_img(fullpath, target_size=(800, 800))
+    # convert image to the array
     img_data = image.img_to_array(img)
+    # format array to be input to VGG16
     img_data = np.expand_dims(img_data, axis=0)
     img_data = preprocess_input(img_data)
-
+    # feed forward image through model 
     inception_feature = model.predict(img_data)
     inception_feature_np = np.array(inception_feature)
+    # flatten features and add to the growing list of samples 
     inception_feature_list.append(inception_feature_np.flatten())
 
 len(inception_feature)
 
+# save huge list of features
 import pickle
 with open('inception_feature', 'wb') as f:
       pickle.dump(inception_feature_list, f)
-
+      
+# rename to avoid alterning raw data and only use 500 data points due to computation time
 X=inception_feature_list[0:500]
 
 
-
+# PCA used to visualize differences in 
 pca=PCA(n_components=2)
-
+# fit to all features
 principalComponents=pca.fit_transform(X)
-
+# create a dataframe to store eigenvectors
 principalDf = pd.DataFrame(data = principalComponents
              , columns = ['principal component 1', 'principal component 2'])
 filenum=files[0:500]
 filenums=[w.replace('.JPG', '') for w in filenum]
-
+# add file for index of file names
 principalDf['Fish ID Number']=filenums
 principalDf.head()
 
--- FeatureExtract_VGG16.py
+++ FeatureExtract_VGG16.py
@@ -13,49 +13,56 @@ import pandas as pd
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
+# read in pretrained model and remove softmax layer
 model = VGG16(weights='imagenet', include_top=False)
 model.summary()
 
+# empty list to add features for each sample to
 vgg16_feature_list = []
 main_dir='C:/Users/w10007346/Dropbox/CNN/Unsupervised_Set/'
 from os import listdir
 from os.path import isfile, join
-
+# get the file names from the directory
 files=[f for f in listdir(main_dir) if isfile(join(main_dir, f))]
 
 for file in files:
     fullpath=main_dir+file
     img = image.load_img(fullpath, target_size=(800, 800))
+    # convert image to the array
     img_data = image.img_to_array(img)
+    # format array to be input to VGG16
     img_data = np.expand_dims(img_data, axis=0)
     img_data = preprocess_input(img_data)
-
+    # feed forward image through model 
     vgg16_feature = model.predict(img_data)
     vgg16_feature_np = np.array(vgg16_feature)
+    # flatten features and add to the growing list of samples 
     vgg16_feature_list.append(vgg16_feature_np.flatten())
 
 len(vgg16_feature_list)
 
+# save huge list of features
 import pickle
 with open('three_age_list', 'wb') as f:
       pickle.dump(vgg16_feature_list, f)
 
+# rename to avoid alterning raw data
 X=vgg16_feature_list
 
-
+# PCA used to visualize differences in 
 pca=PCA(n_components=2)
-
+# fit to all features
 principalComponents=pca.fit_transform(X)
-
+# create a dataframe to store eigenvectors
 principalDf = pd.DataFrame(data = principalComponents
              , columns = ['principal component 1', 'principal component 2'])
 filenum=files
 filenums=[w.replace('.JPG', '') for w in filenum]
-
+# add file for index of file names
 principalDf['Fish ID Number']=filenums
 principalDf.head()
 
-# get ages from csv
+# Get ages from cvs
 al=pd.read_csv('C:/Users/w10007346/Dropbox/CNN/al_scales.csv')
 ms=pd.read_csv('C:/Users/w10007346/Dropbox/CNN/ms_scales.csv')
 tx=pd.read_csv('C:/Users/w10007346/Dropbox/CNN/tx_scales.csv')
@@ -91,9 +98,7 @@ plt.show()
 fig.savefig('VGG16_PCA.png', dpi=800, bbox_inches="tight")
 
 
-len(labels)
-len(scatter_x)
-
+# apply Kmeans algorithm to find groups of samples independent of true ages
 from sklearn.cluster import KMeans
 kmeans=KMeans(n_clusters=3)
 kmeans.fit(X)
